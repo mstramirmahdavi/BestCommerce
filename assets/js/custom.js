fa0909119af40e50e059a024a42df9c547057fb6
mstr_amirmahdavi <seyedamir_mahdavi@yahoo.com>
2019-01-29 16:37:09 +0330

    hero slider , hot-offer , productcard is done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,4 +21,23 @@ $(document).ready(function () {
       }
     }
   });
+  $("#hot-offer").owlCarousel({
+    loop: true,
+    dots: false,
+    nav: true,
+    slideSpeed: 2000,
+    navText: ['<span class="icon-left-arrow"></span>','<span class="icon-right-arrow"></span>'],
+    rewindSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
 });
